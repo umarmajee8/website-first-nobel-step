93dfd88978474d8754dfd7844fc8040ed6c184fd
umarmajee8 <koreanuc8@gmail.com>
2026-05-29 04:34:29 +0500

    favicon

--- testDownload.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-async function downloadLogo() {
-  const url = 'https://drive.google.com/uc?export=download&id=17no--1RLs1mzkG2iYfqfcvpWkpKA3c4X';
-  const response = await fetch(url);
-  const arrayBuffer = await response.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-  console.log(buffer.slice(0, 10).toString('hex'));
-}
-downloadLogo();
